b90069b622e6e74542bedec1425407564f09ae77
Is@&J3ra <jqcaraujo@gmail.com>
2020-12-18 01:34:54 -0300

    Configurando para rodar os testes no jenkins-chomer

--- web/features/support/env.rb
+++ web/features/support/env.rb
@@ -16,6 +16,18 @@ when "chrome"
 when "firefox_headless"
   @driver = :selenium_headless
 when "chrome_headless"
+  Capybara.register_driver :selenium_chrome_headless do |app|
+    Capybara::Selenium::Driver.load_selenium
+    browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+      opts.args << "--headless"
+      opts.args << "--disable-gpu"
+      opts.args << "--no-sandbox"
+      opts.args << "--desable-dev-shm-usage"
+      opts.args << "--disable-site-isolation-trials"
+    end
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+  end
+
   @driver = :selenium_chrome_headless
 else
   raise "Variável de @drive está vazia, não é possível abrir navegador."
